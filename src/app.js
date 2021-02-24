426e8026208cb7ead67a0d9ae32213285cab3204
Lohith C <lohithlohi1895@gmail.com>
2021-02-24 17:37:05 +0530

    Initial commit.

---
+++ src/app.js
@@ -0,0 +1,104 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+
+const app = express();
+
+//Define paths for express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewPath = path.join(__dirname, "../templates/views");
+const partialPath = path.join(__dirname, "../templates/partials");
+
+//Setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewPath);
+hbs.registerPartials(partialPath);
+
+//Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Lohith C",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About me",
+    name: "Lohith C",
+    message:
+      "I am a Full Stack Developer working in Tata Consultancy Services.",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    message: "Please contact - lohithlohi1895@gmail.com for any help",
+    title: "Help",
+    name: "Lohith C",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address",
+    });
+  }
+
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({ error });
+        }
+
+        res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address,
+        });
+      });
+    }
+  );
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term",
+    });
+  }
+  console.log(req.query.search);
+  res.send({
+    products: [],
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Lohith C",
+    errorMessage: "Help artical not found",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Lohith C",
+    errorMessage: "Page not found",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is up on port 3000");
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,28 @@
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    "http://api.weatherstack.com/current?access_key=21e405ed7e6ed483f53ab39e1c7effae&query=" +
+    latitude +
+    "," +
+    longitude +
+    "&units=f";
+
+  request({ url: url, json: true }, (error, response) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (response.body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        response.body.current.weather_descriptions[0] +
+          ". It is currently " +
+          response.body.current.temperature +
+          " degress out."
+      );
+    }
+  });
+};
+
+module.exports = forecast;
